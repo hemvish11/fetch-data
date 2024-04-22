eecb7f0f6328df2ebeaddf3f37766a0afecd2eaa
hemant <hemvish11@gmail.com>
2024-04-23 00:47:18 +0530

    project done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['cdn.fakercloud.com', 'cloudflare-ipfs.com'],
+        unoptimized: true,
+
+    },
+};
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,109 @@
-import Image from "next/image";
+"use client";
+import Image from 'next/image';
+import React, { useEffect, useState } from 'react'
+import { RingLoader } from 'react-spinners';
 
-export default function Home() {
+function Home() {
+
+  const [personData, setPersonData] = useState([]);
+  const [currUserData, setCurrUserData] = useState();
+  const [clicked, setClicked] = useState(false);
+  const [noDataFetched, setNoDataSetched] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const doFetching = async () => {
+    try {
+      let data = await fetch("https://602e7c2c4410730017c50b9d.mockapi.io/users");
+      data = await data.json();
+      console.log(data);
+      setPersonData(data);
+      setLoading(false);
+    } catch (error) {
+      setNoDataSetched(true);
+      throw (error);
+    }
+  }
+
+  useEffect(() => {
+    doFetching();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
+      {
+        noDataFetched ?
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <div className='flex justify-center items-center'>
+            <h1 className='font-semibold text-3xl mt-4'>No Data to Show</h1>
+          </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          :
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          (loading ?
+            <div className='flex items-center justify-center w-full h-screen'>
+              <RingLoader
+                color="#36d7b7"
+                size={150}
+              />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+            </div>
+            : <div>
+              <h1 className='text-2xl font-semibold mt-8 ml-8'>Users</h1>
+              <div className='flex'>
+                <div className={`${clicked ? "md:w-1/2 w-full" : "w-full"} `}>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+                  {personData.map((item, index) => {
+                    return (
+                      <div key={index} className='m-4 border p-4 hover:bg-green-300 cursor-pointer rounded-lg bg-gray-200'
+                        onClick={() => {
+                          setCurrUserData(item);
+                          setClicked(true);
+                        }}
+                      >
+                        <div className='flex gap-2 '>
+                          <Image height={75} width={75} src={item.avatar} alt={`${item.profile.firstName}-pic`} className='rounded-full' />
+                          <div>
+                            <h1 className='font-semibold'>{item.profile.firstName}</h1>
+                            <p className='text-xs text-gray-600 font-semibold'>{item.jobTitle}</p>
+                            <p className='text-xs text-gray-600 font-semibold mt-5'>Click for more info</p>
+                          </div>
+                        </div>
+
+                      </div>
+                    )
+                  })}
+                </div>
+
+                <div className={`${clicked ? "md:w-1/2 fixed w-full top-2 left-2 right-2 md:left-auto md:right-2 md:top-10 border p-4 rounded-lg" : "w-0"} bg-white`}>
+
+                  {clicked &&
+                    <div >
+                      <div className='flex flex-col justify-center items-center'>
+                        <Image height={100} width={100} src={currUserData.avatar} alt={`${currUserData.profile.firstName}-pic`} className='rounded-full' />
+                        <h1 className='font-semibold'>{currUserData.profile.firstName}</h1>
+
+                      </div>
+                      <div className='flex flex-col justify-start items-start'>
+                        <p className='text-gray-900 font-semibold'>First Name: <span className='text-gray-700'>{currUserData.profile.firstName} </span></p>
+                        <p className='text-gray-900 font-semibold'>Last Name: <span className='text-gray-700'>{currUserData.profile.lastName}</span></p>
+                        <p className='text-gray-900 font-semibold'>Job Title: <span className='text-gray-700'>{currUserData.jobTitle}</span></p>
+                        <p className='text-gray-900 font-semibold'>Username: <span className='text-gray-700'>{currUserData.profile.username}</span></p>
+                        <p className='text-gray-900 font-semibold'>Email: <span className='text-gray-700'>{currUserData.profile.email}</span></p>
+                        <p className='text-gray-900 font-semibold'>Bio: <span className='text-gray-700'>{currUserData.Bio}</span></p>
+                      </div>
+
+                    </div>
+                  }
+                </div>
+
+              </div>
+
+            </div>)
+      }
+
     </main>
-  );
+
+  )
 }
+
+export default Home
